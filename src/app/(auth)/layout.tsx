08053b3bd64a1bece54abb3ea982fe3b3a7f7177
PRANAV DESAI <pranav.desai_comp23@pccoer.in>
2025-09-19 18:56:09 +0000

    like this login

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,3 @@
-import { Logo } from "@/components/logo";
-import Link from "next/link";
-
 export default function AuthLayout({
   children,
 }: {
@@ -8,11 +5,6 @@ export default function AuthLayout({
 }) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
-      <div className="absolute left-4 top-4 md:left-8 md:top-8">
-        <Link href="/">
-          <Logo />
-        </Link>
-      </div>
       <main className="w-full flex-1 flex items-center justify-center">
         {children}
       </main>
